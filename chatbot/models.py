f9630b44d87728ff7d05a66c3799501c1b3c8106
Seungwon Park <psw70408@gmail.com>
2023-07-26 16:51:09 +0900

    chatbot model 수정

--- chatbot/models.py
+++ chatbot/models.py
@@ -1,14 +1,14 @@
 from django.db import models
+from django.utils import timezone
 from django.contrib.auth import get_user_model
 
 
 User = get_user_model()
 
 class Conversation(models.Model):
-    questioner = models.ForeignKey(User, on_delete=models.CASCADE)
+    questioner = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     prompt = models.CharField(max_length=512)
     response = models.TextField()
-    asked_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"{self.prompt}: {self.response}"
