c1c2b2b69a91b29f183a34310130a918de116ff6
Seungwon Park <psw70408@gmail.com>
2023-07-26 14:55:08 +0900

    로그인 APIView 생성

--- user/views.py
+++ user/views.py
@@ -3,3 +3,15 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.throttling import UserRateThrottle
 from .serializers import UserSerializer
+
+### Registration
+class Registration(APIView):
+
+    Throttle_classes = [UserRateThrottle]
+
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
