7ad791468db2673ee5ce1e992d7d8a34287fce88
Seungwon Park <psw70408@gmail.com>
2023-07-26 20:07:22 +0900

    conversation model 수정

--- chatbot/models.py
+++ chatbot/models.py
@@ -6,9 +6,10 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 class Conversation(models.Model):
-    questioner = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    questioner = models.ForeignKey(User, on_delete=models.CASCADE)
     prompt = models.CharField(max_length=512)
     response = models.TextField()
-
+    asked_at = models.DateTimeField(auto_now_add=True)
+    
     def __str__(self):
         return f"{self.prompt}: {self.response}"
