096d6550b50d25ebc4dcad04fcbc2595cf987d50
Seungwon Park <psw70408@gmail.com>
2023-07-27 13:19:22 +0900

    chatview 수정

--- chatbot/views.py
+++ chatbot/views.py
@@ -24,8 +24,9 @@ class ChatView(APIView):
         # 유저의 질문 가져오기
         prompt = request.data.get('prompt')
         if prompt:
-            # 이전 대화 기록 가져오기 -> get 이 필요가 없음. 채팅화면은 프론트에서 해줄테니깐
-            session_conversations = request.session.get('conversations', [])
+            # 이전 대화 기록 가져오기
+            user = request.user
+            session_conversations = user.conversation.all()
             previous_conversations = "\n".join([f"User: {c['prompt']}\nAI: {c['response']}" for c in session_conversations])
             prompt_with_previous = f"{previous_conversations}\nUser: {prompt}\nAI:"
 
@@ -40,13 +41,14 @@ class ChatView(APIView):
             )
             response = completions.choices[0].text.strip()
 
-            conversation = Conversation(prompt=prompt, response=response)
+            conversation = Conversation(questioner=user, prompt=prompt, response=response)
             conversation.save()
 
-            # 대화 기록에 새로운 응답 추가
-            session_conversations.append({'prompt': prompt, 'response': response})
-            request.session['conversations'] = session_conversations
-            request.session.modified = True
+            # 대화 기록에 새로운 응답 추가 더이상 필요하지 않음. 위의 conversation.save() 로 데이터베이스에 대화 기록이 저장되기 때문
+            
+            # session_conversations.append({'prompt': prompt, 'response': response})
+            # request.session['conversations'] = session_conversations
+            # request.session.modified = True
 
             return Response(status=status.HTTP_201_CREATED)
         
